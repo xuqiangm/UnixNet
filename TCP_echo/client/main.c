ee6cf9debf745630d4851f9ea8e7c1663b23254d
xuqiangm <xuqiangm@gmail.com>
2016-05-11 17:37:26 +0800

    echo server and client

---
+++ TCP_echo/client/main.c
@@ -0,0 +1,73 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define MAXLINE 1024
+
+void str_cli(FILE* fp, int sockfd);
+int readline(int sockfd, void* ptr, int maxline);
+
+int main(int argc, char** argv){
+	int sockfd;
+	struct sockaddr_in servaddr;
+
+	if(argc != 2){
+		perror("usage:tcpcli <IPaddress>");
+		exit(1);
+	}
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = 9877;
+	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+
+	if(connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1){
+		perror("connect failed!");
+	}
+
+	str_cli(stdin,sockfd);
+	return 0;
+}
+
+void str_cli(FILE* fp, int sockfd){
+	char sendline[MAXLINE], recvline[MAXLINE];
+
+	while(fgets(sendline,MAXLINE,fp) != NULL){
+		write(sockfd, sendline, strlen(sendline));
+		readline(sockfd, recvline, MAXLINE);
+		printf("from server:");
+		fputs(recvline,stdout);
+	}
+}
+
+int readline(int sockfd, void* vptr, int maxline){
+	char c,* ptr=vptr;
+	int n;
+	for(n=1; n<maxline; n++){
+		ssize_t nread = read(sockfd,&c,1);
+		if(nread == -1){
+			perror("read error;");
+			close(sockfd);
+			return -1;
+		}
+		else if(nread = 0){
+			printf("success.");
+			close(sockfd);
+			return n-1;
+		}
+		else{
+			*ptr++ = c;
+			if(c == '\n')
+				break;
+		}
+	}
+	*ptr = '\0';
+	return n;
+}
---
+++ TCP_echo/server/main.c
@@ -0,0 +1,56 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define IPADDRESS "127.0.0.1"
+
+void str_echo(int connfd);
+
+int main(int argc,char** argv){
+	int listenfd,connfd;
+	pid_t childpid;
+	struct sockaddr_in cliaddr,servaddr;
+	listenfd = socket(AF_INET,SOCK_STREAM,0);
+
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = 9877;
+	if(!inet_pton(AF_INET, IPADDRESS, &servaddr.sin_addr)){
+		perror("ip format error.");
+		exit(1);
+	}
+	if(bind(listenfd,(struct sockaddr*)&servaddr,sizeof(servaddr)) == -1){
+		perror("bind error");
+		exit(1);
+	}
+
+	listen(listenfd,10);
+
+	int clilen = sizeof(cliaddr);
+	while(1){
+		connfd = accept(listenfd, (struct sockaddr*)&cliaddr, &clilen);
+		childpid = fork();
+		if(childpid == 0){	//child process
+			close(listenfd);
+			str_echo(connfd);
+			exit(0);
+		}
+		close(connfd);
+	}
+	return 0;
+}
+
+void str_echo(int connfd){
+	char buf[1024];
+	size_t n;
+	while((n = read(connfd, buf, sizeof(buf))) > 0){
+		printf("from client:%.*s\n",(int)n,buf);
+		write(connfd, buf, n);
+	}
+}
