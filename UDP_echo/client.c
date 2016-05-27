3a8aee4e46d963843fdc7241374e4f0320b3c372
xuqiangm <xuqiangm@gmail.com>
2016-05-27 12:50:06 +0800

    UDP 验证接受到的响应

--- UDP_echo/client.c
+++ UDP_echo/client.c
@@ -12,6 +12,7 @@
 #define MAXLINE 1024
 
 void dg_cli(FILE*, int , struct sockaddr*, socklen_t);
+char * sock_ntop(const struct sockaddr *sa, socklen_t salen);
 
 int main(int argc, char** argv){
 	int sockfd;
@@ -35,14 +36,43 @@ int main(int argc, char** argv){
 
 void dg_cli(FILE* fp, int sockfd, struct sockaddr* servaddr, socklen_t servlen){
 	int n;
-	char sendline[MAXLINE],recvline[MAXLINE];
+	char sendline[MAXLINE],recvline[MAXLINE+1];
+	socklen_t len;
+	struct sockaddr* preply_addr;
+
+	preply_addr = malloc(servlen);
 
 	while(fgets(sendline, MAXLINE, fp) != NULL){
 		sendto(sockfd, sendline, strlen(sendline), 0, servaddr, servlen);
 
-		n = recvfrom(sockfd, recvline, MAXLINE, 0 ,NULL, NULL);
+		n = recvfrom(sockfd, recvline, MAXLINE, 0 ,preply_addr, &len);
+		if(len != servlen || memcmp(servaddr, preply_addr, len)!=0){
+			printf("reply from %s (ignored)\n", sock_ntop(preply_addr, len));
+			continue;
+		}
 
 		recvline[n] = '\0';
 		fputs(recvline, stdout);
+	}
+}
+
+char * sock_ntop(const struct sockaddr *sa, socklen_t salen)
+{
+    char		portstr[8];
+    static char str[128];		/* Unix domain is largest */
+
+	switch (sa->sa_family) {
+		case AF_INET: {
+			struct sockaddr_in	*sin = (struct sockaddr_in *) sa;
+
+			if (inet_ntop(AF_INET, &sin->sin_addr, str, sizeof(str)) == NULL)
+				return(NULL);
+			if (ntohs(sin->sin_port) != 0) {
+				snprintf(portstr, sizeof(portstr), ":%d", ntohs(sin->sin_port));
+				strcat(str, portstr);
+			}
+			return(str);
+		}
+		return NULL;
 	}
 }
